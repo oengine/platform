f1d6e9d0c718eca8088405cfbd7050c24552e896
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-02-16 22:53:59 +0700

    move hook to package laravel base

--- helpers/platform.php
+++ helpers/platform.php
@@ -1,13 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Symfony\Component\Finder\SplFileInfo;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Gate;
-use OEngine\Platform\Facades\Action;
-use OEngine\Platform\Facades\Filter;
 use OEngine\Platform\Facades\Module;
 use OEngine\Platform\Facades\Plugin;
 use OEngine\Platform\Facades\Theme;
@@ -70,94 +67,6 @@ if (!function_exists('platform_route_is_admin')) {
     function platform_route_is_admin()
     {
         return in_array(\OEngine\Platform\Middleware\AdminPlatform::class, Request()->route()->gatherMiddleware());
-    }
-}
-if (!function_exists('add_action')) {
-    /**
-     * @param  string | array  $hook
-     * @param $callback
-     * @param  int  $priority
-     * @param  int  $arguments
-     */
-    function add_action($hook, $callback, int $priority = 20, int $arguments = 1)
-    {
-        Action::addListener($hook, $callback, $priority, $arguments);
-    }
-}
-
-if (!function_exists('remove_action')) {
-    /**
-     * @param  string  $hook
-     */
-    function remove_action($hook, $callback = null)
-    {
-        Action::removeListener($hook, $callback);
-    }
-}
-if (!function_exists('do_action')) {
-    /**
-     * @param  string  $hook
-     */
-    function do_action()
-    {
-        $args = func_get_args();
-        Action::fire(array_shift($args), $args);
-    }
-}
-
-if (!function_exists('add_filter')) {
-    /**
-     * @param  string | array  $hook
-     * @param $callback
-     * @param  int  $priority
-     * @param  int  $arguments
-     */
-    function add_filter($hook, $callback, int $priority = 20, int $arguments = 1)
-    {
-        Filter::addListener($hook, $callback, $priority, $arguments);
-    }
-}
-if (!function_exists('remove_filter')) {
-    /**
-     * @param  string  $hook
-     */
-    function remove_filter($hook, $callback)
-    {
-        Filter::removeListener($hook, $callback);
-    }
-}
-
-if (!function_exists('apply_filters')) {
-    /**
-     * @return mixed
-     */
-    function apply_filters()
-    {
-        $args = func_get_args();
-
-        return Filter::fire(array_shift($args), $args);
-    }
-}
-
-if (!function_exists('get_hooks')) {
-    /**
-     * @param  string|null  $name
-     * @param  bool  $isFilter
-     * @return array
-     */
-    function get_hooks(?string $name = null, bool $isFilter = true): array
-    {
-        if ($isFilter) {
-            $listeners = Filter::getListeners();
-        } else {
-            $listeners = Action::getListeners();
-        }
-
-        if (empty($name)) {
-            return $listeners;
-        }
-
-        return Arr::get($listeners, $name, []);
     }
 }
 
